e9a303714bf2b70c0bfd8d50e0a20ebc1949798b
Fireflake <vsknight@gmx.at>
2014-09-23 19:12:35 +0200

    - adds a small module to display the server name

---
+++ app/code/local/Tech4Africa/ServerName/Block/Page/Html/Welcome.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fireflake
+ * Date: 23.09.14
+ * Time: 19:07
+ */ 
+class Tech4Africa_ServerName_Block_Page_Html_Welcome extends Mage_Page_Block_Html_Welcome {
+    /**
+     * Get block messsage
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        if (empty($this->_data['welcome'])) {
+            if (Mage::isInstalled() && Mage::getSingleton('customer/session')->isLoggedIn()) {
+                $this->_data['welcome'] = $this->__('Welcome, %s!', $this->escapeHtml(Mage::getSingleton('customer/session')->getCustomer()->getName()));
+            } else {
+                $this->_data['welcome'] = "This page was generated on: " . $_SERVER["SERVER_NAME"];
+            }
+        }
+
+        return $this->_data['welcome'];
+    }
+
+}
---
+++ app/code/local/Tech4Africa/ServerName/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fireflake
+ * Date: 23.09.14
+ * Time: 19:03
+ */ 
+class Tech4Africa_ServerName_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
